8e5f5b1bad71853787b0354075cd5f9ce47bfb19
yunus kibar <yunuskibaar@gmail.com>
2023-07-02 22:15:32 +0300

    commit 1

---
+++ app/app.js
@@ -0,0 +1,241 @@
+
+
+
+
+let TopButton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      TopButton.style.display = "block";
+    } else {
+      TopButton.style.display = "none";
+      
+    }
+  }
+  TopButton.style.transition= "2s"
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+  function openBizKimiz(){
+    var bizkimiz=document.getElementById("bizKimizId");
+    bizkimiz.style.display="block";
+  }
+  function closeBizKimiz(){
+
+    var bizkimiz=document.getElementById("bizKimizId");
+    bizkimiz.style.display="none";
+  }
+
+  function openPopupP2() {
+    var popup = document.getElementById("popupP2");
+    popup.style.display = "block";
+  }
+  
+  function closePopupP2() {
+    var popup = document.getElementById("popupP2");
+    popup.style.display = "none";
+  }
+
+
+
+  function openPopup1() {
+    var popup = document.getElementById("popup1");
+    popup.style.display = "block";
+  }
+  
+  function closePopup1() {
+    var popup = document.getElementById("popup1");
+    popup.style.display = "none";
+  }
+  
+
+
+  function openPopup2() {
+    var popup = document.getElementById("popup2");
+    popup.style.display = "block";
+  }
+  
+  function closePopup2() {
+    var popup = document.getElementById("popup2");
+    popup.style.display = "none";
+  }
+
+
+  function openPopup3() {
+    var popup = document.getElementById("popup3");
+    popup.style.display = "block";
+  }
+  
+  function closePopup3() {
+    var popup = document.getElementById("popup3");
+    popup.style.display = "none";
+  }
+
+
+  function openPopup21() {
+    var popup = document.getElementById("popup21");
+    popup.style.display = "block";
+  }
+  
+  function closePopup21() {
+    var popup = document.getElementById("popup21");
+    popup.style.display = "none";
+  }
+
+  function openPopup22() {
+    var popup = document.getElementById("popup22");
+    popup.style.display = "block";
+  }
+  
+  function closePopup22() {
+    var popup = document.getElementById("popup22");
+    popup.style.display = "none";
+  }
+
+  function openPopup23() {
+    var popup = document.getElementById("popup23");
+    popup.style.display = "block";
+  }
+  
+  function closePopup23() {
+    var popup = document.getElementById("popup23");
+    popup.style.display = "none";
+  }
+
+  function openPopup24() {
+    var popup = document.getElementById("popup24");
+    popup.style.display = "block";
+  }
+  
+  function closePopup24() {
+    var popup = document.getElementById("popup24");
+    popup.style.display = "none";
+  }
+  function openPopup31() {
+    var popup = document.getElementById("popup31");
+    popup.style.display = "block";
+  }
+  
+  function closePopup31() {
+    var popup = document.getElementById("popup31");
+    popup.style.display = "none";
+  }
+
+  function openPopup32() {
+    var popup = document.getElementById("popup32");
+    popup.style.display = "block";
+  }
+  
+  function closePopup32() {
+    var popup = document.getElementById("popup32");
+    popup.style.display = "none";
+  } 
+  
+  
+  function openPopup33() {
+    var popup = document.getElementById("popup33");
+    popup.style.display = "block";
+  }
+  
+  function closePopup33() {
+    var popup = document.getElementById("popup33");
+    popup.style.display = "none";
+  } 
+  
+  
+  function openPopup34() {
+    var popup = document.getElementById("popup34");
+    popup.style.display = "block";
+  }
+  
+  function closePopup34() {
+    var popup = document.getElementById("popup34");
+    popup.style.display = "none";
+  } 
+  
+  function openPopup41() {
+    var popup = document.getElementById("popup41");
+    popup.style.display = "block";
+  }
+  
+  function closePopup41() {
+    var popup = document.getElementById("popup41");
+    popup.style.display = "none";
+  } 
+  
+  
+  function openPopup42() {
+    var popup = document.getElementById("popup42");
+    popup.style.display = "block";
+  }
+  
+  function closePopup42() {
+    var popup = document.getElementById("popup42");
+    popup.style.display = "none";
+  }
+
+  function openPopup43() {
+    var popup = document.getElementById("popup43");
+    popup.style.display = "block";
+  }
+  
+  function closePopup43() {
+    var popup = document.getElementById("popup43");
+    popup.style.display = "none";
+  }
+
+  function openPopup44() {
+    var popup = document.getElementById("popup44");
+    popup.style.display = "block";
+  }
+  
+  function closePopup44() {
+    var popup = document.getElementById("popup44");
+    popup.style.display = "none";
+  }
+
+  function openPopup51() {
+    var popup = document.getElementById("popup51");
+    popup.style.display = "block";
+  }
+  
+  function closePopup51() {
+    var popup = document.getElementById("popup51");
+    popup.style.display = "none";
+  }
+
+  function openPopup52() {
+    var popup = document.getElementById("popup52");
+    popup.style.display = "block";
+  }
+  
+  function closePopup52() {
+    var popup = document.getElementById("popup52");
+    popup.style.display = "none";
+  }
+
+  function openPopup53() {
+    var popup = document.getElementById("popup53");
+    popup.style.display = "block";
+  }
+  
+  function closePopup53() {
+    var popup = document.getElementById("popup53");
+    popup.style.display = "none";
+  }
+
+  function openPopup54() {
+    var popup = document.getElementById("popup54");
+    popup.style.display = "block";
+  }
+  
+  function closePopup54() {
+    var popup = document.getElementById("popup54");
+    popup.style.display = "none";
+  }
